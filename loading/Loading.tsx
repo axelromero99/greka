9ab140960bea82d315c9a1496a5af6ff96f67515
arrecode <axelromero99@outlook.com>
2022-07-10 03:33:51 -0300

    fix: deleted everything... pull from development

--- loading/Loading.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {Image} from "@chakra-ui/react";
-
-const Loading = () => {
-  return <Image left={"43%"} position={"absolute"} src={"/assets/loading.svg"} top={"43vh"} />;
-};
-
-export default Loading;
